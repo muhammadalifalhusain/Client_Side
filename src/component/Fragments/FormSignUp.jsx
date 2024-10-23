9984f9a9d679ca9cea6f39c5ffaf7f677f37e1f3
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-10-23 18:13:43 +0700

    update

---
+++ src/component/Fragments/FormSignUp.jsx
@@ -0,0 +1,38 @@
+import Button from "../elements/button";
+import LabeledInput from "../elements/LabeledInput";
+
+const FormSignUp = () => {
+  return (
+    <form action="">
+      <div className="mb-6">
+        <LabeledInput
+          label="Name"
+          type="text"
+          placeholder="John Doe"
+          name="name"
+        />
+      </div>
+      <div className="mb-6">
+        <LabeledInput
+          label="Email Address"
+          type="email"
+          placeholder="hello@example.com"
+          name="email"
+        />
+      </div>
+      <div className="mb-6">
+        <LabeledInput
+          label="Password"
+          type="password"
+          placeholder="*************"
+          name="password"
+        />
+      </div>
+      <Button variant="bg-[#299D91] w-full text-white" type="submit">
+        Sign Up
+      </Button>
+    </form>
+  );
+};
+
+export default FormSignUp;
