6d967765f68e672844aa17326500f5b608cf5ac5
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-10-24 12:35:56 +0700

    updateInput

--- src/component/Fragments/FormSignUp.jsx
+++ src/component/Fragments/FormSignUp.jsx
@@ -8,7 +8,7 @@ const FormSignUp = () => {
         <LabeledInput
           label="Name"
           type="text"
-          placeholder="Husain Babi"
+          placeholder="YourName"
           name="name"
         />
       </div>
